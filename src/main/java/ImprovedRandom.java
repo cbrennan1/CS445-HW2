562a537b0eb304c6c6cd4a9470fa09027cb77639
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-23 17:01:01 -0600

    initialization of ImprovedRandom and ImprovedRandomTesting Classes

---
+++ src/main/java/ImprovedRandom.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public class ImprovedRandom {
+
+	public ImprovedRandom() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/testing/java/ImprovedRandomTesting.java
@@ -0,0 +1,9 @@
+package testing.java;
+
+public class ImprovedRandomTesting {
+
+	public ImprovedRandomTesting() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
