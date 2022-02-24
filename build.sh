cdda25c3541f2989b0bf3877fa572eb169832cbf
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-24 01:35:34 -0600

    build.sh

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ sudo apt-get -y install openjdk-17-jdk
 sudo apt-get install -y junit
 
 sudo snap install gradle --classic
-./gradlew clean build
-./gradlew jacocoTestReport
+sudo ./gradlew clean build
+sudo ./gradlew jacocoTestReport
